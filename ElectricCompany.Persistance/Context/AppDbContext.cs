15c55d3be0db7f91529a0a40b3fb735438828a5e
emir <emircan_snmez@outlook.com>
2025-11-20 21:43:12 +0300

    Entity Framework Integrated and Db Connection added

---
+++ ElectricCompany.Persistance/Context/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ElectricCompany.Persistance.Context
+{
+    public sealed class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options): base(options) {}
+    }
+}
--- ElectricCompany.WebAPI/Program.cs
+++ ElectricCompany.WebAPI/Program.cs
@@ -1,15 +1,29 @@
+using ElectricCompany.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+string connectionString = builder.Configuration.GetConnectionString("NpgsqlConnectionString");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString(connectionString);
+});
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddControllers().AddApplicationPart(typeof(ElectricCompany.Presentation.AssemblyReference).Assembly);
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapScalarApiReference();
 }
+
 app.UseRouting();
+
 app.MapControllerRoute(name: "", pattern: "{controller}/{action}");
 
 app.Run();
