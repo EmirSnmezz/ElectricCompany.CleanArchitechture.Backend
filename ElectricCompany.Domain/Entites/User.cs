2bc1b62353729bad31365f4bc0fdaea72f2c1179
emir <emircan_snmez@outlook.com>
2025-11-20 23:14:05 +0300

    user entity and base entity class created

---
+++ ElectricCompany.Domain/Entites/User.cs
@@ -0,0 +1,13 @@
+﻿using ElectricCompany.Domain.Abstractions;
+
+namespace ElectricCompany.Domain.Entites
+{
+    public sealed class User : Entity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
