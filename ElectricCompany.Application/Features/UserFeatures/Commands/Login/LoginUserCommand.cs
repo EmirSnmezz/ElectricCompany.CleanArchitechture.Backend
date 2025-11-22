645bc3ddc9492b82d6cea302f97d84765dd8907d
emir <emircan_snmez@outlook.com>
2025-11-22 15:55:29 +0300

    Login user  DTO added

---
+++ ElectricCompany.Application/Features/UserFeatures/Commands/Login/LoginUserCommand.cs
@@ -0,0 +1,7 @@
+﻿using ElectricCompany.Domain.DTOS;
+using MediatR;
+
+namespace ElectricCompany.Application.Features.UserFeatures.Commands.Login
+{
+    public sealed record LoginUserCommand(string userNameOrEmail, string password) : IRequest<MessageResponse>;
+}
---
+++ ElectricCompany.Application/Features/UserFeatures/Commands/Login/LoginUserCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using ElectricCompany.Domain.DTOS;
+using MediatR;
+
+namespace ElectricCompany.Application.Features.UserFeatures.Commands.Login
+{
+    public sealed class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, MessageResponse>
+    {
+        public async Task<MessageResponse> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            return new("Kullanıcı girişi başarılı");
+        }
+    }
+}
