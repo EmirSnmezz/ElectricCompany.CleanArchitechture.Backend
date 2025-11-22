92af5039a8876a826310873a44a67068d947d28d
emir <emircan_snmez@outlook.com>
2025-11-22 17:33:30 +0300

    User Controller and abstraction of User Controller created

---
+++ ElectricCompany.Presentation/Controllers/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectricCompany.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class ApiController : ControllerBase
+    {
+        public readonly IMediator _mediator;
+
+        protected ApiController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
---
+++ ElectricCompany.Presentation/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using ElectricCompany.Application.Features.UserFeatures.Commands.Register;
+using ElectricCompany.Application.Features.UserFeatures.Queries.UserQueries.GetAllUser;
+using ElectricCompany.Domain.DTOS;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectricCompany.Presentation.Controllers
+{
+    public sealed class UsersController : ApiController
+    {
+        public UsersController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Register(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            MessageResponse response = await _mediator.Send(request, cancellationToken);
+            return Created("", response);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetAllUser()
+        {
+            GetAllUserQuery request = new();
+            GetAllUserQueryResponse response = await _mediator.Send(request);
+            return Ok(response);
+        }
+    }
+}
